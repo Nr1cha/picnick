9f4ff95e8519ddc2abae6531af1c25f8b03b8b8c
Nr1cha <vrm.richards@gmail.com>
2025-07-14 16:04:14 -0600

    added question componant and basic styling

---
+++ src/components/questionBox.jsx
@@ -0,0 +1,15 @@
+"use client";
+import '../styles/questionBox.css'
+
+function pollQuestion() {
+
+    return (
+        <>
+            <div className="pollQuestion">
+                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit?</p>
+            </div>
+        </>
+    )
+}
+// test
+export default pollQuestion
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./styles/selection.css"
 import UserSelection from "./components/selection.jsx"
+import PollQuestion from "./components/questionBox.jsx"
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <PollQuestion />
+    <UserSelection />
+    <UserSelection />
+    <UserSelection />
     <UserSelection />
   </StrictMode>,
 )
