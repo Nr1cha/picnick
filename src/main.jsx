783bb554e78e572948d04be71a33e10217102ab5
Nr1cha <vrm.richards@gmail.com>
2025-07-15 11:41:36 -0600

    updated styling to establish base grid gap and added txt file for static testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./styles/selection.css"
+import "./index.css"
 import UserSelection from "./components/selection.jsx"
 import PollQuestion from "./components/questionBox.jsx"
 
