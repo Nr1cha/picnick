58737f2d940fc2969da002cc9340bf7e109a29fe
Nr1cha <vrm.richards@gmail.com>
2025-07-15 22:34:58 -0600

    set up for componant change

--- src/components/selection.jsx
+++ src/components/selection.jsx
@@ -1,11 +1,26 @@
 "use client";
 import '../styles/selection.css'
+// import React, {useState} from 'react';
+
+
+  // const optionParent = () => {
+  //   const [progressBar, setProgressBar] = useState({
+  //     backgroundColor: "lightblue",
+  //   });
+
+  //   const handleClick = () => {
+  //     setProgressBar({
+  //       backgroundColor: "green",
+  //     });
+  //   };
+  // }
+
 
 function UserSelection() {
 
   return (
     <>
-      <div className="option">
+      <div className="option"/* onClick={handleClick}*/>
         <span>
           <button className="btnOption">Vote Here</button>
         </span>
